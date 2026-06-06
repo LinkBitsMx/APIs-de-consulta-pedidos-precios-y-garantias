7b17023ed0270919d5c5d9d8f46258fdfab72318
Fernando Hernandez <fernando.hdez.marq.22@gmail.com>
2026-06-06 12:59:19 -0600

    habilitar Swagger en todos los entornos y descomentar redirección HTTPS

--- ApisConsulta.Api/Program.cs
+++ ApisConsulta.Api/Program.cs
@@ -59,13 +59,12 @@ builder.Services.AddInfrastructureDependencies(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+//app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
